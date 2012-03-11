38a122620fe1db2965128b3cc0b745ed89e5f632
Shawn Mclean <mixmasterxp@gmail.com>
2012-03-11 18:34:38 -0500

    added a shared file for use on both client and server

---
+++ src/shared/shared.coffee
@@ -0,0 +1,8 @@
+#shared code between the client and the server
+((exports) ->
+  UserStatus =
+    Away: 0
+    Back: 1
+    Visible: 2
+    Hidden: 3
+) (if typeof exports is "undefined" then this["Chatsy"] = {} else exports)
---
+++ src/shared/shared.js
@@ -0,0 +1,13 @@
+(function() {
+
+  (function(exports) {
+    var UserStatus;
+    return UserStatus = {
+      Away: 0,
+      Back: 1,
+      Visible: 2,
+      Hidden: 3
+    };
+  })((typeof exports === "undefined" ? this["Chatsy"] = {} : exports));
+
+}).call(this);
